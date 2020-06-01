a1e56e38c895b0a658d556b060230af26522c933
Zackery Mowrer <Zackmowrer@gmail.com>
2020-06-01 08:46:22 -0400

    Remove now-redundant comments

--- inc/quiz.php
+++ inc/quiz.php
@@ -20,15 +20,6 @@ $index = null;
 // Declare variable to hold the current question, initialized to null
 $question = null;
 
-/*
-    If the server request was of type POST
-        Check if the user's answer was equal to the correct answer.
-        If it was correct:
-            1. Assign a congratulutory string to the toast variable
-            2. Ancrement the session variable that holds the total number correct by one.
-        Otherwise:
-            1. Assign a bummer message to the toast variable.
-*/
 // If the server request was of type POST,
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Get the user's answer and the index of the previous question
@@ -59,27 +50,6 @@ if (!isset($_SESSION["used_indices"])) {
     $showScore = false;
 }
 
-/*
-  If the number of used indexes in our session variable is equal to the total number of questions
-  to be asked:
-        1.  Reset the session variable for used indexes to an empty array 
-        2.  Set the show score variable to true.
-
-  Else:
-    1. Set the show score variable to false 
-    2. If it's the first question of the round:
-        a. Set a session variable that holds the total correct to 0. 
-        b. Set the toast variable to an empty string.
-        c. Assign a random number to a variable to hold an index. Continue doing this
-            for as long as the number generated is found in the session variable that holds used indexes.
-        d. Add the random number generated to the used indexes session variable.      
-        e. Set the individual question variable to be a question from the questions array and use the index
-            stored in the variable in step c as the index.
-        f. Create a variable to hold the number of items in the session variable that holds used indexes
-        g. Create a new variable that holds an array. The array should contain the correctAnswer,
-            firstIncorrectAnswer, and secondIncorrect answer from the variable in step e.
-        h. Shuffle the array from step g.
-*/
 // If we have displayed all the questions,
 if (count($_SESSION["used_indices"]) === $totalQuestions) {
     // Reset used indices session variable to empty array
